290cdf75cbf589aab14a41e353de2681e9c3f56b
harikc123 <63502924+harikc123@users.noreply.github.com>
2022-11-11 13:11:52 +0545

    Update a.py

--- a.py
+++ a.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.header('Have a Nice Journey and warm stay in China')
 
 if st.button('Hello everyone'):
-     st.write('I am going to China for PHD!!!!')
+     st.write('I am going to China for PHD in Scholarship, I will sent you 100$ monthly')
 else:
      st.write('Click me!!!')
