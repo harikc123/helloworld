179a9e8696c001a587cd158142fb2c81f731ae86
harikc123 <63502924+harikc123@users.noreply.github.com>
2023-03-04 22:56:25 +0545

    Update a.py

--- a.py
+++ a.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.header('Welcome Everyone!!!')
 
-if st.button('Hello everyone'):
+if st.button('Hello'):
      st.write('A Data Analyst from Nepal')
 else:
      st.write('Click me!!!')
